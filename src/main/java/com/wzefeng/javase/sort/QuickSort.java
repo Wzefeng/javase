354d4828f0f5dd4bae59cc796693a3b7ddb8f09b
Wzefeng <zefeng_wang@126.com>
2019-05-13 16:31:32 +0800

    快速排序增加注释

--- src/main/java/com/wzefeng/javase/sort/QuickSort.java
+++ src/main/java/com/wzefeng/javase/sort/QuickSort.java
@@ -17,21 +17,36 @@ public class QuickSort {
         if (startIndex >= endIndex) {
             return;
         }
-        int pivotIndex = partition(arr, startIndex, endIndex);
+        int pivotIndex = partitionSwapAndReturnIndex(arr, startIndex, endIndex);
         quickSort(arr, startIndex, pivotIndex - 1);
         quickSort(arr, pivotIndex + 1, endIndex);
     }
 
-    private static int partition(int[] arr, int startIndex, int endIndex) {
+    /**
+     * 数组填坑并返回基准索引。
+     *
+     * @param arr
+     * @param startIndex
+     * @param endIndex
+     * @return
+     */
+    private static int partitionSwapAndReturnIndex(int[] arr, int startIndex, int endIndex) {
+        // 取分区第一个元素为基准值
         int pivot = arr[startIndex];
-        int left = startIndex;
-        int right = endIndex;
+        // 基准索引
         int index = startIndex;
+        // 左边指针
+        int left = startIndex;
+        // 右边指针
+        int right = endIndex;
 
         while (right >= left) {
+            // 由于选择左边值为基准值，所以要从右边指针开始遍历
             while (right >= left) {
                 if (arr[right] < pivot) {
+                    // 填坑
                     arr[index] = arr[right];
+                    // 新的坑的索引
                     index = right;
                     left++;
                     break;
@@ -40,7 +55,9 @@ public class QuickSort {
             }
             while (right >= left) {
                 if (arr[left] > pivot) {
+                    // 填坑
                     arr[index] = arr[left];
+                    // 新的坑的索引
                     index = left;
                     right--;
                     break;
