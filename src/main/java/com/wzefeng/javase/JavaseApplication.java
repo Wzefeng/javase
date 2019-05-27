5a65136ecaf50d4e9e1394b85abfad87f0a1b582
Wzefeng <zefeng_wang@126.com>
2019-05-27 10:04:43 +0800

    lambda

--- src/main/java/com/wzefeng/javase/JavaseApplication.java
+++ src/main/java/com/wzefeng/javase/JavaseApplication.java
@@ -3,7 +3,7 @@ package com.wzefeng.javase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class JavaseApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/wzefeng/javase/lambda/Main.java
@@ -0,0 +1,20 @@
+package com.wzefeng.javase.lambda;
+
+import java.util.function.*;
+
+/**
+ * @author wangzefeng
+ * @date 2019/5/17 15:20
+ */
+public class Main {
+
+    /**
+     * <p>{@code Supplier<T> T get();}  {@link Supplier#get()}
+     * <p>{@code Consumer<T> void accept(T t);} {@link Consumer#accept(Object)}
+     * <p>{@code Predicate<T> boolean test(T t);} {@link Predicate#test(Object)}
+     * <p>{@code Function<T,R> R apply(T t);} {@link Function#apply(Object)}
+     */
+    public static void main(String[] args) {
+
+    }
+}
